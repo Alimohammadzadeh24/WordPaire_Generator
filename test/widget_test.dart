83fdc6acd81ff465b6e10e3d9ec6f328e7cf5482
Ali.M <ali24m@ali.m>
2021-09-20 15:55:02 +0430

    nothig

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:wordpair_generator/main.dart';
+import 'package:tmp1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
